f279b9fbd0f9e2eebc20b07b21f4b7d262a342d1
Youhei Kirino <kirino.yohei@gmail.com>
2026-09-22 19:25:34 +0900

    refactor: move item effects owner to TypeScript

---
+++ tests/node/fixtures/typescript/item_effects_partial_input.ts
@@ -0,0 +1,13 @@
+import { ITEM_EFFECTS } from "../../../../src/systems/item_effects";
+
+const healCharacter = { name: "回復役", hp: 10, maxHp: 30 };
+ITEM_EFFECTS.HEAL_POTION({ char: healCharacter, party: [healCharacter], rng: Math.random });
+
+const manaCharacter = { name: "術者", mp: 1, maxMp: 3 };
+ITEM_EFFECTS.MANA_POTION({ char: manaCharacter, party: [manaCharacter], rng: Math.random });
+
+const statusOnlyCharacter = { name: "状態役", status: "poisoned" };
+ITEM_EFFECTS.ANTIDOTE({ char: statusOnlyCharacter, party: [statusOnlyCharacter], rng: Math.random });
+
+const buffCharacter = { name: "強化役", buffs: [] };
+ITEM_EFFECTS.STR_POTION({ char: buffCharacter, party: [buffCharacter], rng: Math.random });
